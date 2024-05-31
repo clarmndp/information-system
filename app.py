45e4140aca1e2ae936d9da388cf9ab93956ace3a
camanzanido <camanzanido@up.edu.ph>
2024-05-31 12:01:03 +0800

    feat: created login page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import *
 import mariadb
 
 # function to connect to the database
@@ -21,6 +22,7 @@ def connect_db(user_type):
             port=3306,
             database="food"
         )
+        messagebox.showinfo("Database Connection", f"Successfully connected to the database as {user_type}.")
         return conn
     except mariadb.Error as e:
         messagebox.showerror("Database Connection Error", f"Error connecting to MariaDB Platform: {e}")
@@ -36,9 +38,43 @@ def connect_db(user_type):
     #add your features here
 
 
-
 # Tkinter application
 root = tk.Tk()
 root.title("Food Information System")
+
+name_var = tk.StringVar()
+passw_var = tk.StringVar()
+
+def submit():
+    name = name_var.get()
+    password = passw_var.get()
+    print("The name is : " + name)
+    print("The password is : " + password)
+    name_var.set("")
+    passw_var.set("")
+
+    if name == 'foodie' and password == 'chefP!':
+        admin = connect_db("admin")
+    if name == 'pares' and password == 'diwataP':
+        casual_conn = connect_db("casual")
+
+# Login Page
+myLabel = Label(root, text="LogIn", font=('calibre', 10, 'bold'))
+myLabel.grid(row=0, column=0, columnspan=2, pady=10)
+
+name_label = tk.Label(root, text='Username', font=('calibre', 10, 'bold'))
+name_entry = tk.Entry(root, textvariable=name_var, font=('calibre', 10, 'normal'))
+
+passw_label = tk.Label(root, text='Password', font=('calibre', 10, 'bold'))
+passw_entry = tk.Entry(root, textvariable=passw_var, font=('calibre', 10, 'normal'), show='*')
+
+sub_btn = tk.Button(root, text='Submit', command=submit)
+
+name_label.grid(row=1, column=0, pady=5)
+name_entry.grid(row=1, column=1, pady=5)
+passw_label.grid(row=2, column=0, pady=5)
+passw_entry.grid(row=2, column=1, pady=5)
+sub_btn.grid(row=3, column=1, pady=10)
+
 
 root.mainloop()
